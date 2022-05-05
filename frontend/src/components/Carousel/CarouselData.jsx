6ca80adc0eace01bcf0c4583f60f95f452a4cf2e
YashrajKulkarni <yashrajkulkarni2001@gmail.com>
2022-05-05 18:11:32 +0530

    FEA: Added Carousel

---
+++ frontend/src/components/Carousel/CarouselData.jsx
@@ -0,0 +1,20 @@
+export const CarouselData = [
+    {
+        Image: 'https://m.media-amazon.com/images/I/61YuJJ7PMwL._SX3000_.jpg'
+    },
+    {
+        Image: 'https://m.media-amazon.com/images/I/61MtKclTZIL._SX3000_.jpg'
+    },
+    {
+        Image: 'https://m.media-amazon.com/images/I/61yWNrPmB-L._SX3000_.jpg'
+    },
+    {
+        Image: 'https://m.media-amazon.com/images/I/61BFazitqUL._SX3000_.jpg'
+    },
+    {
+        Image: 'https://m.media-amazon.com/images/I/610Tq8M7P+L._SX3000_.jpg'
+    },
+    {
+        Image: 'https://m.media-amazon.com/images/I/61Bo0+AnJoL._SX3000_.jpg'
+    }
+]
